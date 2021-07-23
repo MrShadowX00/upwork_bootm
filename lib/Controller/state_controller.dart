4de7e4b30b096bf0078cf348de56b1a33025cc53
MrShadowX00 <khujaking@gmail.com>
2021-07-23 10:56:13 +0500

    master

--- lib/Controller/state_controller.dart
+++ lib/Controller/state_controller.dart
@@ -6,84 +6,93 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:upwork_bottom/Screens/main_screen.dart';
 
-class StateController extends GetxController{
-  List<Widget> list= [];
+class StateController extends GetxController {
+  List<Widget> list = [];
   static ScrollController scrollController = ScrollController();
   double changeValue = 5000;
   int startIndex = 0;
   int endIndex = 5000;
-  String? currentIndex='0';
-  static Widget steps= Container();
-
+  String? currentIndex = '0';
+  static Widget steps = Container();
 
   @override
   void onInit() {
-    list.add(Container(
-      width: 200,
-    ),);
-    if(!MainScreen.isLimited) {
+    list.add(
+      Container(
+        width: 200,
+      ),
+    );
+    if (!MainScreen.isLimited) {
       for (int k = 0; k < 5000; k++) {
         if (k % 5 == 0) {
           print(k);
-          list.add(Padding(
-            padding: const EdgeInsets.all(3),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(2),
-                  color: Colors.white
+          list.add(
+            Padding(
+              padding: const EdgeInsets.all(3),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(2),
+                    color: Colors.white),
+                height: 20,
+                width: 2,
               ),
-              height: 20,
-              width: 2,
             ),
-          ),);
+          );
         } else {
-          list.add(Padding(
-            padding: const EdgeInsets.all(3),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
+          list.add(
+            Padding(
+              padding: const EdgeInsets.all(3),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                height: 10,
+                width: 1,
               ),
-              height: 10,
-              width: 1,
             ),
-          ),);
+          );
         }
       }
-    }else{
+    } else {
       startIndex = 2500;
       endIndex = 6500;
       currentIndex = '2500';
-      for (int k = 0; k < 4000; k++) {
+      for (int k = 0; k < 4001; k++) {
         if (k % 5 == 0) {
           print(k);
-          list.add(Padding(
-            padding: const EdgeInsets.all(3),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(2),
-                  color: Colors.white
+          list.add(
+            Padding(
+              padding: const EdgeInsets.all(3),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(2),
+                    color: Colors.white),
+                height: 20,
+                width: 2,
               ),
-              height: 20,
-              width: 2,
             ),
-          ),);
+          );
         } else {
-          list.add(Padding(
-            padding: const EdgeInsets.all(3),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
+          list.add(
+            Padding(
+              padding: const EdgeInsets.all(3),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                height: 10,
+                width: 1,
               ),
-              height: 10,
-              width: 1,
             ),
-          ),);
+          );
         }
       }
     }
-    list.add(Container(
-      width: 200,
-    ),);
+    list.add(
+      Container(
+        width: 200,
+      ),
+    );
     scrollController.addListener(controllerListener);
     changeState();
     print('init');
@@ -95,11 +104,11 @@ class StateController extends GetxController{
     super.onClose();
   }
 
-  increment(){
-    if(MainScreen.isLimited){
+  increment() {
+    if (MainScreen.isLimited) {
       scrollController.jumpTo(scrollController.position.maxScrollExtent);
       currentIndex = '6500';
-    }else {
+    } else {
       startIndex = startIndex + 5000;
       endIndex = endIndex + 5000;
       currentIndex = startIndex.toString();
@@ -109,12 +118,12 @@ class StateController extends GetxController{
     update();
   }
 
-  decrement(){
-    if(startIndex != 0){
-      if(MainScreen.isLimited){
+  decrement() {
+    if (startIndex != 0) {
+      if (MainScreen.isLimited) {
         currentIndex = '2500';
         scrollController.jumpTo(scrollController.position.minScrollExtent);
-      }else {
+      } else {
         startIndex = startIndex - 5000;
         endIndex = endIndex - 5000;
         currentIndex = endIndex.toString();
@@ -125,43 +134,43 @@ class StateController extends GetxController{
     }
   }
 
-
-  changeState() async{
+  changeState() async {
     print('$endIndex');
-    if(MainScreen.isLimited){
+    if (MainScreen.isLimited) {
       if (currentIndex != '2500') {
-        scrollController = ScrollController(
-            initialScrollOffset: 5000
-        );
+        scrollController = ScrollController(initialScrollOffset: 5000);
         scrollController.addListener(controllerListener);
-
       }
-    }else {
+    } else {
       if (currentIndex != '0') {
-        scrollController = ScrollController(
-            initialScrollOffset: 2500
-        );
+        scrollController = ScrollController(initialScrollOffset: 2500);
         scrollController.addListener(controllerListener);
       }
       print('chaqirildi');
     }
-    steps =  SingleChildScrollView(
+    steps = SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       controller: scrollController,
       child: Row(
-        children:list,
+        children: list,
       ),
     );
 
     update();
   }
 
-  controllerListener(){
+  controllerListener() {
     // print(scrollController.position.pixels);
-    if(MainScreen.isLimited){
-      currentIndex =
-          (startIndex + (scrollController.position.pixels ~/ 7.2)).toString();
-    }else {
+    if (MainScreen.isLimited) {
+      if (double.parse(currentIndex!) > endIndex) {
+        currentIndex = endIndex.toString();
+      } else if (double.parse(currentIndex!) < startIndex) {
+        currentIndex = startIndex.toString();
+      } else {
+        currentIndex =
+            (startIndex + (scrollController.position.pixels ~/ 7.2)).toString();
+      }
+    } else {
       if (scrollController.position.pixels ==
           scrollController.position.maxScrollExtent) {
         startIndex = startIndex + 5000;
@@ -175,8 +184,8 @@ class StateController extends GetxController{
             children: list,
           ),
         );
-        scrollController.position.jumpTo(
-            scrollController.position.minScrollExtent + 200);
+        scrollController.position
+            .jumpTo(scrollController.position.minScrollExtent + 200);
       } else if (scrollController.position.pixels ==
           scrollController.position.minScrollExtent) {
         if (startIndex != 0) {
@@ -191,12 +200,12 @@ class StateController extends GetxController{
               children: list,
             ),
           );
-          scrollController.position.jumpTo(
-              scrollController.position.maxScrollExtent - 200);
+          scrollController.position
+              .jumpTo(scrollController.position.maxScrollExtent - 200);
         } else {
           if (changeValue > endIndex.toDouble()) {
-            scrollController.position.jumpTo(
-                scrollController.position.maxScrollExtent + 200);
+            scrollController.position
+                .jumpTo(scrollController.position.maxScrollExtent + 200);
           }
         }
         changeValue = endIndex.toDouble();
@@ -206,5 +215,4 @@ class StateController extends GetxController{
     }
     update();
   }
-
 }
